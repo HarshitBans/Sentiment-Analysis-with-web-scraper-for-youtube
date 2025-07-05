6ef915eeec59ca685c32c8d5b7218e4b4f9b9aa0
Harshit Bansal <114908428+HarshitBans@users.noreply.github.com>
2025-07-05 20:38:30 +0530

    Update app.py

--- Sentiment Analysis with web scraper for youtube/app.py
+++ Sentiment Analysis with web scraper for youtube/app.py
@@ -62,7 +62,7 @@ def get_results():
     return jsonify(classified_result)
 
 if __name__ == '__main__':
-     port = int(os.environ.get('PORT', 5000))  # fallback to 5000 for local dev
+    port = int(os.environ.get('PORT', 5000))  # fallback to 5000 for local dev
     app.run(host='0.0.0.0', port=port)
 
 
